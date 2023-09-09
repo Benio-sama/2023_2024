c1f57c584afdb5fd19475c04a44abb100b40d3c4
Benio-sama <vago.kata.viki@gmail.com>
2023-09-09 21:06:59 +0200

    ASD

---
+++ Kalandor/Kalandor/Feladatok.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalandor
+{
+	internal class Feladatok
+	{
+
+		public string Jatek(Kalandor k)
+		{
+			return "";
+		}
+		public void Veszelyes(Kalandor k)
+		{
+			Random rnd = new Random();
+			int kocka = rnd.Next(1, 7);
+			while (kocka == 1)
+			{
+                Console.WriteLine("talalkoztal egy nagy ehes tigrissel, megserultel, -8 hp");
+				k.Hp -= 8;
+				kocka = rnd.Next(0, 7);
+			}
+			while (kocka == 2)
+			{
+				Console.WriteLine("atkeltel egy veszelyes folyon, elragadott az aramlat, -8 hp");
+				k.Hp -= 8;
+				kocka = rnd.Next(0, 7);
+			}
+			while (kocka == 3)
+			{
+				Console.WriteLine("megbotlottal egy csapdaban, megserultel, -8 hp");
+				k.Hp -= 8;
+				kocka = rnd.Next(0, 7);
+			}
+			while (kocka == 4)
+			{
+				Console.WriteLine("eltevedtel a dzsungelban, -8 hp");
+				k.Hp -= 8;
+				kocka = rnd.Next(0, 7);
+			}
+			while (kocka == 5)
+			{
+				Console.WriteLine("megtamadt egy mergezo kigyo, -8 hp");
+				k.Hp -= 8;
+				kocka = rnd.Next(0, 7);
+			}
+
+		}
+		public string Biztonsagos(Kalandor k)
+		{
+			return "";
+		}
+	}
+}
---
+++ Kalandor/Kalandor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalandor
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Kalandor k = new Kalandor("Mici");
+			Feladatok f = new Feladatok();
+			f.Veszelyes(k);
+
+
+
+            Console.ReadKey();
+        }
+	}
+}
