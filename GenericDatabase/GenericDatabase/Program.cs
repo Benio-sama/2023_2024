aaf64411db392b67a1093dbbb3a895fd42527bd4
Benio-sama <vago.kata.viki@gmail.com>
2023-11-20 23:48:40 +0100

    hazi kesz

--- GenericDatabase/GenericDatabase/Program.cs
+++ GenericDatabase/GenericDatabase/Program.cs
@@ -10,6 +10,31 @@ namespace GenericDatabase
     {
         static void Main(string[] args)
         {
+            DatabaseManager<Student> students = new DatabaseManager<Student>();
+            DatabaseManager<Book> books = new DatabaseManager<Book>();
+            DatabaseManager<Employee> employees = new DatabaseManager<Employee>();
+
+            students.AddRecord(new Student(3, "tomi", 21));
+            students.AddRecord(new Student(3, "asd", 14));
+            students.AddRecord(new Student(1, "mici", 19));
+            students.AddRecord(new Student(4, "bali", 24));
+
+            employees.AddRecord(new Employee(1, "marton", "ceo"));
+            employees.AddRecord(new Employee(2, "mate", "cto"));
+            employees.AddRecord(new Employee(3, "kata", "trainee"));
+
+            books.AddRecord(new Book(1, "asd", "asd"));
+            books.AddRecord(new Book(2, "dsa", "dsa"));
+            books.AddRecord(new Book(3, "ewq", "qwe"));
+
+            books.RemoveRecord(1);
+            students.GetRecord(3);
+
+            students.PrintDatabase();
+            books.PrintDatabase();
+            employees.PrintDatabase();
+
+            Console.ReadKey();
         }
     }
 }
--- GenericRemote/GenericRemote/TV.cs
+++ GenericRemote/GenericRemote/TV.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GenericRemote
 {
-    internal class TV
+    internal class TV : Device
     {
         private bool status = false;
         private int channel = 1;
@@ -19,6 +19,34 @@ namespace GenericRemote
         public override string ToString()
         {
             return "TV: status: " + (this.status ? "ON" : "OFF") + $", channel: {this.channel}, volume: {this.volume}";
+        }
+        public void PowerOn()
+        {
+            this.status = true;
+            Console.WriteLine("a tv bekapcsolva");
+        }
+        public void PowerOff()
+        {
+            this.status = false;
+            Console.WriteLine("a tv kikapcsolva");
+        }
+        public void SetChannel(int channel)
+        {
+            this.channel = channel;
+            Console.WriteLine($"csatorna beallitva");
+        }
+        public void AdjustVolume(bool change)
+        {
+            if (change)
+            {
+                this.volume += 10;
+                Console.WriteLine($"felhangositva, a hangero: {this.volume}");
+            }
+            else
+            {
+                this.volume -= 10;
+                Console.WriteLine($"lehalkitva, a hangero: {this.volume}");
+            }
         }
     }
 }
