a6e293105f724123261b88275bb53359e409a47a
Benio-sama <vago.kata.viki@gmail.com>
2023-11-21 12:57:11 +0100

    nem mukodik es nemtudom hogy mi a hiba :')

---
+++ Nepesseg/Nepesseg/Feladatok.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Nepesseg
+{
+    internal class Feladatok
+    {
+        private List<Orszag> orszagok = new List<Orszag>();
+
+        internal List<Orszag> Orszagok { get => orszagok; set => orszagok = value; }
+
+        public void Beolvas(string fajl)
+        {
+            using (StreamReader sr = new StreamReader(fajl))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string sor = sr.ReadLine();
+                    string[] sorok = sor.Split(';');
+                    string nev = sorok[0];
+                    int terulet = int.Parse(sorok[1]);
+                    string nepesseg = sorok[2];
+                    long nep;
+                    if (nepesseg[nepesseg.Length - 1].Equals("g"))
+                    {
+                        sorok[2] = sorok[2].Remove(sorok[2].Length - 1);
+                        nep = int.Parse(sorok[2]);
+                        nep *= 10000;
+                    }
+                    else
+                    {
+                        nep = int.Parse(sorok[2]);
+                    }
+                    string fovaros = sorok[3];
+                    int fovnep = int.Parse(sorok[4]);
+                    Orszag o = new Orszag(nev, terulet, nep, fovaros, fovnep);
+                    orszagok.Add(o);
+                }
+                foreach(var item in orszagok)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
---
+++ Nepesseg/Nepesseg/Orszag.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nepesseg
+{
+    internal class Orszag
+    {
+        public string Orszagnev { get; private set; }
+        public int Terulet { get; private set; }
+        public long Nepesseg { get; private set; }
+        public string Fovaros { get; private set; }
+        public int FovarosNepesseg { get; private set; }
+
+        public Orszag(string orszagnev, int terulet, long nepesseg, string fovaros, int fovarosNepesseg)
+        {
+            Orszagnev = orszagnev;
+            Terulet = terulet;
+            Nepesseg = nepesseg;
+            Fovaros = fovaros;
+            FovarosNepesseg = fovarosNepesseg;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Orszagnev}: \nterulete: {this.Terulet}, \nnepessege: {this.Nepesseg} \nfovarosa: {this.Fovaros} \nfovaros nepessege: {this.FovarosNepesseg}";
+        }
+    }
+}
