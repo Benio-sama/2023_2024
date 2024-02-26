c9b2d9b641eaaeab656c46d064208909b1545494
Benio-sama <vago.kata.viki@gmail.com>
2024-02-26 23:54:29 +0100

    edes jo istenem segits meg

--- Adatbazisdoga/Adatbazisdoga/Adatbazis.cs
+++ Adatbazisdoga/Adatbazisdoga/Adatbazis.cs
@@ -9,14 +9,12 @@ namespace Adatbazisdoga
 {
     internal class Adatbazis
     {
-        private Dictionary<int, Album> albums = new Dictionary<int, Album>();
-        private List<Album> albumok = new List<Album>();
-        private Dictionary<int, Toplista> toplistas = new Dictionary<int, Toplista>();
-        private List<Toplista> toplistak = new List<Toplista>();
+        private Dictionary<Album, int> albums = new Dictionary<Album, int>();
+        private Dictionary<Toplista, int> toplistas = new Dictionary<Toplista, int>();
         MySqlConnectionStringBuilder conn = new MySqlConnectionStringBuilder();
 
-        internal Dictionary<int, Album> Albums { get => albums; set => albums = value; }
-        internal Dictionary<int, Toplista> Toplistas { get => toplistas; set => toplistas = value; }
+        internal Dictionary<Album, int> Albums { get => albums; set => albums = value; }
+        internal Dictionary<Toplista, int> Toplistas { get => toplistas; set => toplistas = value; }
 
         public Adatbazis() 
         {
@@ -52,8 +50,7 @@ namespace Adatbazisdoga
                         string eloado = reader.GetString("eloado");
                         string cim = reader.GetString("cim");
                         Album a = new Album(eloado, cim);
-                        albums.Add(id, a);
-                        albumok.Add(a);
+                        albums.Add(a, id);
                     }
                 }
                 /*foreach (var item in albums)
@@ -94,8 +91,7 @@ namespace Adatbazisdoga
                         int ev = reader.GetInt32("ev");
                         string kiado = reader.GetString("kiado");
                         Toplista t = new Toplista(helyezes, platinadb, ev, kiado);
-                        toplistas.Add(albumid, t);
-                        toplistak.Add(t);
+                        toplistas.Add(t, albumid);
                     }
                 }
                 /*foreach (var item in toplistas)
@@ -110,9 +106,9 @@ namespace Adatbazisdoga
         }
         public void Fekete()
         {
-            foreach (var item in albumok)
+            foreach (var item in albums)
             {
-                if (item.Eloado.ToLower().Contains("fekete") || item.Cim.ToLower().Contains("fekete"))
+                if (item.Key.Eloado.ToLower().Contains("fekete") || item.Key.Cim.ToLower().Contains("fekete"))
                 {
                     Console.WriteLine(item);
                 }
@@ -121,9 +117,9 @@ namespace Adatbazisdoga
         public void Statisztika()
         {
             Dictionary<string,int> stat = new Dictionary<string,int>();
-            foreach (var item in toplistak)
+            foreach (var item in toplistas)
             {
-                string kulcs = item.Kiado;
+                string kulcs = item.Key.Kiado;
                 if (!stat.ContainsKey(kulcs))
                 {
                     stat.Add(kulcs, 0);
@@ -139,24 +135,118 @@ namespace Adatbazisdoga
         {
             //Adja  meg  lekérdezés  segítségével  azt  az  előadót,  aki  a  legtöbb  platinalemez-elismerést  kapta!
             Dictionary<string, int> stat = new Dictionary<string,int>();
-            foreach (var item in toplistak)
+            foreach (var item in albums)
             {
-                string kulcs = item.Kiado;
-                if (!stat.ContainsKey(kulcs))
+                string kulcs = item.Key.Eloado;
+                int id = item.Value;
+                int plat = 0;
+                foreach (var item1 in toplistas)
                 {
-                    stat.Add(kulcs, 0);
+                    if (item1.Value == id)
+                    {
+                        plat += item1.Key.Platinadb;
+                    }
+                }
+                if (stat.ContainsKey(kulcs))
+                {
+                    stat[kulcs] += plat;
+                }
+                else
+                {
+                    stat.Add(kulcs, plat);
+                }
+
+            }
+            string nev = "asd";
+            int max = 0;
+            foreach (var item in stat)
+            {
+                if (item.Value > max)
+                {
+                    max = item.Value;
+                    nev = item.Key;
                 }
             }
-
+            Console.WriteLine(nev + " - " + max);
         }
         public void NevCim()
         {
-            foreach (var item in albumok)
+            foreach (var item in albums)
             {
-                if (item.Cim.ToLower().Contains(item.Eloado.ToLower()))
+                if (item.Key.Cim.ToLower().Contains(item.Key.Eloado.ToLower()))
                 {
                     Console.WriteLine(item);
                 }
+            }
+        }
+        public void SzereplesDB()
+        {
+            Dictionary<string, int> stat = new Dictionary<string, int>();
+            foreach (var item in albums)
+            {
+                string kulcs = $"{item.Key.Eloado} - {item.Key.Cim}";
+                int id = item.Value;
+                int db = 0;
+                foreach (var item1 in toplistas)
+                {
+                    if (item1.Value == id)
+                    {
+                        db ++;
+                    }
+                }
+                if (stat.ContainsKey(kulcs))
+                {
+                    stat[kulcs] += db;
+                }
+                else
+                {
+                    stat.Add(kulcs, db);
+                }
+            }
+            foreach (var item in stat)
+            {
+                if (item.Value > 2)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+        public void PalyaBea()
+        {
+            Dictionary<string, string> stat = new Dictionary<string, string>();
+            foreach (var item in albums)
+            {
+                if (item.Key.Eloado == "Palya Bea")
+                {
+                    foreach (var item1 in toplistas)
+                    {
+                        if (item.Value == item1.Value)
+                        {
+                            string val = item1.Key.Kiado;
+                            foreach (var item2 in toplistas)
+                            {
+                                if (item2.Key.Kiado == val)
+                                {
+                                    foreach (var item3 in albums)
+                                    {
+                                        if (item2.Value == item3.Value)
+                                        {
+                                            string kulcs = item3.Key.Eloado;
+                                            if (!stat.ContainsKey(kulcs))
+                                            {
+                                                stat.Add(kulcs, val);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            foreach (var item in stat)
+            {
+                Console.WriteLine(item);
             }
         }
     }
